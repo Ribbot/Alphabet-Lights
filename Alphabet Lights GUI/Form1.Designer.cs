2287ec59759aa8ca7c9ed6aac9697d1524d695da
Ribbot <1234blair@gmail.com>
2018-12-16 20:38:06 -0800

    Initial Commit

--- Alphabet Lights GUI/Form1.Designer.cs
+++ Alphabet Lights GUI/Form1.Designer.cs
@@ -29,12 +29,124 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(134, 48);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "On";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.On_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(134, 113);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Off";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Off_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(63, 225);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 35);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Color 1";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Choose_Color1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(152, 225);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 35);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Color 2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Choose_Color2_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(240, 225);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 35);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Color 3";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Choose_Color3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(63, 324);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(257, 26);
+            this.textBox1.TabIndex = 6;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(127, 370);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(128, 35);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Send Phrase";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.Phrase_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(398, 643);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Alphabet Lights";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button6;
     }
 }
 
--- Alphabet Lights GUI/Form1.cs
+++ Alphabet Lights GUI/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,83 @@ namespace Alphabet_Lights_GUI
 {
     public partial class Form1 : Form
     {
+        static SerialPort serialPort;
+        Color color1 = Color.Fuchsia, color2 = Color.Cyan, color3 = Color.Green; // Initialize colors with default values
+
+
         public Form1()
         {
             InitializeComponent();
+
+            serialPort = new SerialPort("COM7", 9600); //Initialize serial communication with Arduino over USB
+
+            updateColors();
+        }
+
+
+        private void On_Click(object sender, EventArgs e)
+        {
+            serialPort.Open();
+            serialPort.WriteLine("1"); //Send "On" command
+            serialPort.Close();
+        }
+
+
+        private void Off_Click(object sender, EventArgs e)
+        {
+            serialPort.Open();
+            serialPort.WriteLine("2"); //Send "Off" command
+            serialPort.Close();
+        }
+
+
+        private void Choose_Color1_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                color1 = colorDialog1.Color; //Change color1 to color selected by user
+                updateColors();
+            }
+        }
+
+
+        private void Choose_Color2_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                color2 = colorDialog1.Color; //Change color2 to color selected by user
+                updateColors();
+            }
+        }
+
+        private void Choose_Color3_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                color3 = colorDialog1.Color; //Change color3 to color selected by user
+                updateColors();
+            }
+        }
+
+        private void updateColors()
+        {
+            // Change each button background to match its selected color
+            button3.BackColor = color1;
+            button4.BackColor = color2;
+            button5.BackColor = color3;
+
+            serialPort.Open();
+            serialPort.WriteLine("3 " + color1.R + " " + color1.G + " " + color1.B + " " + color2.R + " " + color2.G + " " + color2.B + " " + color3.R + " " + color3.G + " " + color3.B); //Send color selection command
+            serialPort.Close();
+        }
+
+        private void Phrase_Click(object sender, EventArgs e)
+        {
+            string phrase = textBox1.Text;
+
+            serialPort.Open();
+            serialPort.WriteLine("4 " + phrase); //Send phrase command
+            serialPort.Close();
         }
     }
 }
